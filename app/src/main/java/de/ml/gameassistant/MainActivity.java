838cba5c2bf145793d25802f217c0a71fc48e3e4
Fabian Hastenpflug <fabian.hastenpflug@gmail.com>
2019-02-07 20:08:29 +0100

    Menü

--- app/src/main/java/de/ml/gameassistant/MainActivity.java
+++ app/src/main/java/de/ml/gameassistant/MainActivity.java
@@ -4,24 +4,131 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.hardware.SensorEventListener;
 
-public class MainActivity extends AppCompatActivity {
-//hello
-    //und so
-    //hallllllllo
-    //nochmalhallo
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
-    private ImageButton button1;
+    private ImageButton button1, button2, button3, button4;
+    private SensorManager sm_acc;
+    private Sensor acc;
+
+    private long then = 0;
+    private long then2 = 0;
+
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = findViewById(R.id.imageButton);
+
+        sm_acc = (SensorManager) getSystemService(SENSOR_SERVICE);
+        acc = sm_acc.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+
+        button1.setVisibility(View.VISIBLE);
+        button2.setVisibility(View.GONE);
+        button3.setVisibility(View.GONE);
+        button4.setVisibility(View.GONE);
+
+
+
+
+
     }
 
-    public void wuerfeln(){
-        Intent intent = new Intent(this, Wuerfeln.class);
-        startActivity(intent);
+    protected void onResume() {
+        super.onResume();
+        //Sensor-Listener werden gestartet (aktiv, wenn App aktiv ist)
+        sm_acc.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        //Sensor-Listener werden gestoppt (wenn App geschlossen oder pausiert wird)
+        sm_acc.unregisterListener(this);
+
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        //Sensor s erhält die Sensodaten als Array-List
+        Sensor s = event.sensor;
+        //Falls Sensor s Daten vom Accelerometer enthält:
+        if (s.getType() == Sensor.TYPE_ACCELEROMETER) {
+            //Werte für X- und Y-Achse werden ausgelesen und in Ints gespeichert. "Z" wird nicht benötigt.
+            int x = Math.round(event.values[0]);
+            int y = Math.round(event.values[1]);
+            //int z = Math.round(event.values[2]);
+            //die aktuelle Zeit wird erfasst
+            long now = System.currentTimeMillis();
+
+            //Falls der X-Wert niedriger ist als -6 ist und mehr als 1 Sekunde vergangen ist, wird voreblättert.
+            if((x < -6 ) && (now - then2) > 1000) {
+                counter++;
+                aktualisieren();
+                then2 = now;
+
+            }
+            //Falls der X-Wert höher als 6 ist und mehr als 1 Sekunde vergangen ist, wird zurückgeblättert.
+            if((x > 6 ) && (now - then2) > 1000) {
+                counter--;
+                aktualisieren();
+                then2 = now;
+
+            }
+
+
+
+            //Falls Sensor s Daten vom Abstandssensor enthält wird zum Tutorial zurückgeblättert (Seite 0) und es wird in einer Textview angezeigt,
+            //ob der Sensor bedeckt oder nicth bedeckt ist.
+        }
+
+    }
+
+
+    public void aktualisieren() {
+        if (counter > 4) {counter = 1;}
+        if (counter < 1) {counter = 4;}
+
+        button1.setVisibility(View.GONE);
+        button2.setVisibility(View.GONE);
+        button3.setVisibility(View.GONE);
+        button4.setVisibility(View.GONE);
+        switch (counter) {
+            case 1:
+                button1.setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                button2.setVisibility(View.VISIBLE);
+                break;
+            case 3:
+                button3.setVisibility(View.VISIBLE);
+                break;
+            case 4:
+                button4.setVisibility(View.VISIBLE);
+                break;
+
+            default:
+                break;
+        }
+
     }
 }
+
+
