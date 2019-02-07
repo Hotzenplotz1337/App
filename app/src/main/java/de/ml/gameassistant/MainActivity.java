0d24bde209726f4f2601947b3838d8a2ba00a339
Mark Linke <mark.linke.1990@googlemail.com>
2019-02-07 16:37:38 +0100

    wuerfeln activity

--- app/src/main/java/de/ml/gameassistant/MainActivity.java
+++ app/src/main/java/de/ml/gameassistant/MainActivity.java
@@ -1,13 +1,24 @@
 package de.ml.gameassistant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 //hello
+
+    private ImageButton button1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.imageButton);
+    }
+
+    public void wuerfeln(){
+        Intent intent = new Intent(this, Wuerfeln.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/de/ml/gameassistant/Wuerfeln.java
@@ -0,0 +1,13 @@
+package de.ml.gameassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Wuerfeln extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wuerfeln);
+    }
+}
